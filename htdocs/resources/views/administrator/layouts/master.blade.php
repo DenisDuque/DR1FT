9026a71c6b3af3ac9e5d47b784428af4ea202dee
MarinaLl <llambrichdev@gmail.com>
2024-02-28 15:27:12 +0100

    pequeñas modificaciones a la vista master

--- htdocs/resources/views/administrator/layouts/master.blade.php
+++ htdocs/resources/views/administrator/layouts/master.blade.php
@@ -4,25 +4,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>@yield('title')</title>
 </head>
-<body>
+<body class="container background-admin-gradient">
     @section('header')
-        <header>
+        <header class="row">
             <nav>
-                <h2>DASHBOARD</h2>
-                <h2>RACES</h2>
-                <h2>SPONSORS</h2>
-                <h2>INSURANCES</h2>
-                <h2>DRIVERS</h2>
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">DASHBOARD</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">RACES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">SPONSORS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">INSURANCES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">DRIVERS</a>
+                    </li>
+                    <li class="nav-item">
+                        <section>
+                            <i class="bi bi-person"></i>
+                            <h3>Administrator</h3>
+                            <i class="bi bi-box-arrow-left"></i>
+                        </section>
+                    </li>
+                </ul>
+                
             </nav>
-            <section>
-                <i class="bi bi-person"></i>
-                <h3>Administrator</h3>
-                <i class="bi bi-box-arrow-left"></i>
-            </section>
         </header>
     @show
     <main class="container">
