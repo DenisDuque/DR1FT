fa1ac3ab1702c6d9dbce8854557e6a3baa6c2f75
MarinaLl <llambrichdev@gmail.com>
2024-02-29 19:27:55 +0100

    eliminar columnas tabla de vistas

--- htdocs/resources/views/administrator/races/index.blade.php
+++ htdocs/resources/views/administrator/races/index.blade.php
@@ -6,16 +6,16 @@
         <a class="btn btn-primary" href="/admin/races/new" role="button">Add Race</a>
     </section>
 
-    <table class="table table-dark table-hover">
+    <table class="table table-dark table-hover rounded">
         <thead>
             <tr>
             <th scope="col">#</th>
             <th scope="col">Name</th>
-            <th scope="col">Description</th>
-            <th scope="col">Map</th>
+            
+            
             <th scope="col">Max. Drivers</th>
             <th scope="col">Length</th>
-            <th scope="col">Banner</th>
+            
             <th scope="col">Date</th>
             <th scope="col">Place</th>
             <th scope="col">Sponsor Cost</th>
@@ -33,11 +33,10 @@
                             <span class="badge rounded-pill bg-warning text-dark">PRO</span>
                         @endif
                     </td>
-                    <td>{{$race->description}}</td>
-                    <td>{{$race->map}}</td>
+                    
                     <td>{{$race->maxParticipants}}</td>
                     <td>{{$race->length}}</td>
-                    <td>{{$race->banner}}</td>
+                    
                     <td>{{$race->date}}</td>
                     <td>{{$race->startingPlace}}</td>
                     <td>{{$race->sponsorCost}}</td>
