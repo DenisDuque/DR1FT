deca616a819a39cdc6abebed04fd91527997410c
DenisDuque <d.duque.dev@gmail.com>
2024-02-29 18:00:41 +0100

    Sesiones administrador

--- htdocs/routes/web.php
+++ htdocs/routes/web.php
@@ -27,31 +27,53 @@ Route::get('/', function () {
 Route::get('/admin', [AdministratorController::class, 'showLogin']);
 Route::post('/admin', [AdministratorController::class, 'auth'])->name('/admin');
 Route::get('/admin/logout', [AdministratorController::class, 'logout'])->name('admin/logout');
-Route::get('/admin/dashboard', [AdministratorController::class, 'showAdministratorPanel'])->name('/admin/dashboard');
+
+
+Route::get('/admin/dashboard', [AdministratorController::class, 'showAdministratorPanel'])
+->name('/admin/dashboard')
+->middleware('admin.auth');
 
 // ADMINISTRATOR: RACES
-Route::get('/admin/races', [RaceController::class, 'index'])->name('/admin/races');
-Route::get('/admin/races/new', [RaceController::class, 'new']);
-Route::post('/admin/races/new', [RaceController::class, 'create'])->name('/admin/races/new');
-Route::get('/admin/races/edit/{id}', [RaceController::class, 'edit'])->name('admin.races.edit');
-Route::post('/admin/races/edit/{id}', [RaceController::class, 'update'])->name('admin.races.update');
+Route::get('/admin/races', [RaceController::class, 'index'])->name('/admin/races')
+->middleware('admin.auth');
+Route::get('/admin/races/new', [RaceController::class, 'new'])
+->middleware('admin.auth');
+Route::post('/admin/races/new', [RaceController::class, 'create'])->name('/admin/races/new')
+->middleware('admin.auth');
+Route::get('/admin/races/edit/{id}', [RaceController::class, 'edit'])->name('admin.races.edit')
+->middleware('admin.auth');
+Route::post('/admin/races/edit/{id}', [RaceController::class, 'update'])->name('admin.races.update')
+->middleware('admin.auth');
 
 // ADMINISTRATOR: DRIVERS
-Route::get('/admin/drivers', [DriverController::class, 'index'])->name('/admin/drivers');
-Route::get('/admin/drivers/new', [DriverController::class, 'new']);
-Route::post('/admin/drivers/new', [DriverController::class, 'create'])->name('/admin/drivers/new');
-Route::get('/admin/drivers/edit/{id}', [DriverController::class, 'edit'])->name('admin.drivers.edit');
-Route::post('/admin/drivers/edit/{id}', [DriverController::class, 'update'])->name('admin.drivers.update');
+Route::get('/admin/drivers', [DriverController::class, 'index'])->name('/admin/drivers')
+->middleware('admin.auth');
+Route::get('/admin/drivers/new', [DriverController::class, 'new'])
+->middleware('admin.auth');
+Route::post('/admin/drivers/new', [DriverController::class, 'create'])->name('/admin/drivers/new')
+->middleware('admin.auth');
+Route::get('/admin/drivers/edit/{id}', [DriverController::class, 'edit'])->name('admin.drivers.edit')
+->middleware('admin.auth');
+Route::post('/admin/drivers/edit/{id}', [DriverController::class, 'update'])->name('admin.drivers.update')
+->middleware('admin.auth');
 
 // ADMINISTRATOR: INSURANCES
-Route::get('/admin/insurances', [InsuranceController::class, 'index'])->name('/admin/insurances');
-Route::get('/admin/insurances/new', [InsuranceController::class, 'new']);
-Route::post('/admin/insurances/new', [InsuranceController::class, 'create'])->name('/admin/insurances/new');
-Route::get('/admin/insurances/edit/{id}', [InsuranceController::class, 'edit'])->name('admin.insurances.edit');
-Route::post('/admin/insurances/edit/{id}', [InsuranceController::class, 'update'])->name('admin.insurances.update');
+Route::get('/admin/insurances', [InsuranceController::class, 'index'])->name('/admin/insurances')
+->middleware('admin.auth');
+Route::get('/admin/insurances/new', [InsuranceController::class, 'new'])
+->middleware('admin.auth');
+Route::post('/admin/insurances/new', [InsuranceController::class, 'create'])->name('/admin/insurances/new')
+->middleware('admin.auth');
+Route::get('/admin/insurances/edit/{id}', [InsuranceController::class, 'edit'])->name('admin.insurances.edit')
+->middleware('admin.auth');
+Route::post('/admin/insurances/edit/{id}', [InsuranceController::class, 'update'])->name('admin.insurances.update')
+->middleware('admin.auth');
 
 
 // ADMINISTRATOR: SPONSORS
-Route::get('/admin/sponsors', [SponsorController::class, 'index'])->name('/admin/sponsors');
-Route::get('/admin/sponsors/new', [SponsorController::class, 'new']);
-Route::post('/admin/sponsors/new', [SponsorController::class, 'create'])->name('/admin/sponsors/new');
+Route::get('/admin/sponsors', [SponsorController::class, 'index'])->name('/admin/sponsors')
+->middleware('admin.auth');
+Route::get('/admin/sponsors/new', [SponsorController::class, 'new'])
+->middleware('admin.auth');
+Route::post('/admin/sponsors/new', [SponsorController::class, 'create'])->name('/admin/sponsors/new')
+->middleware('admin.auth');
