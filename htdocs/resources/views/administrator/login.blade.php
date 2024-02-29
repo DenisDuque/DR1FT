f2bd199846455e44612205b6bbf3e3d7d4f65ee6
DenisDuque <d.duque.dev@gmail.com>
2024-02-29 10:52:19 +0100

    Empezado CSS login

--- htdocs/resources/views/administrator/login.blade.php
+++ htdocs/resources/views/administrator/login.blade.php
@@ -4,11 +4,15 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>DR1FT Administrator Login</title>
-  <link rel="stylesheet" href="adminLogin.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 
-<body class="main-bg">
+<body class="container background-admin-gradient">
   <!-- Login Form -->
   <div class="container">
     @if (session('error'))
@@ -45,8 +49,8 @@
                 <input type="checkbox" class="form-check-input" name="remember" value="{{old('remember')}}"/>
                 <label for="remember" class="form-label">Remember Me</label>
               </div>
-              <div class="col-12">
-                <button type="submit" class="btn btn-primary">Login</button>
+              <div class="d-grid">
+                <button type="submit" class="btn btn-primary">Sign in</button>
               </div>
             </form>
           </div>
