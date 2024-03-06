f0a47e8f95f6123f9fafb0604a269da17647a718
MarinaLl <llambrichdev@gmail.com>
2024-03-06 12:59:37 +0100

    navegacion races

--- htdocs/resources/views/administrator/races/new.blade.php
+++ htdocs/resources/views/administrator/races/new.blade.php
@@ -1,28 +1,35 @@
 @extends('administrator.layouts.master')
 
-@if ($errors->any())
-    @foreach ($errors->all() as $error)
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            <strong>Error: </strong> {{$error}}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-    @endforeach
-@endif
 
 @section('content')
-    <div class="row mt-5 text-white">
-        <div class="col-lg-5 col-md-12">
+<div class="row mt-5 text-white">
+    <div class="col-lg-5 col-md-12">
+        banner | map
+    </div>
+    <div class="col-lg-7 col-md-12 ">
+        <div class="row my-2">
             <h1 class="admin-form-title">Create Race</h1>
-            <small>Encourage everyone to push their limits.</small>
-            <div class="row">
-                banner | map
-                
-            </div>
+            <small>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</small>
+            @if ($errors->any())
+                @foreach ($errors->all() as $error)
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <strong>Error: </strong> {{$error}}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                @endforeach
+            @endif
         </div>
-        <div class="col-lg-7 col-md-12 ">
-            <div class="row bg-admin p-5 card">
-                     
-                <form class="row g-2" action="{{route('admin.races.new')}}" method="POST" enctype="multipart/form-data">
+        <div class="row bg-admin card">
+                <div class="col-12 shadow p-3">
+                    <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="#">DETAILS</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">INSURANCES</li>
+                            <li class="breadcrumb-item active" aria-current="page">SPONSORS</li>
+                        </ol>
+                    </nav>
+                </div>
+                <form class="row g-2 px-5 pb-4" action="{{route('admin.races.new')}}" method="POST" enctype="multipart/form-data">
                     @csrf
                     <div class="col-md-8">
                         <label for="raceName" class="form-label">Name</label>
